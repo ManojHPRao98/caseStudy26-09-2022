90d8c69c98f84b612b4e5ad70c442487ee9cfc86
Manoj H P <88942179+ManojHPRao98@users.noreply.github.com>
2022-09-26 16:22:49 +0530

    Add files via upload

---
+++ MarksException.java
@@ -0,0 +1,10 @@
+package com.gl.caseStudy1;
+
+public class MarksException extends RuntimeException {
+	static final long serialVersionUTD=2L;
+	public MarksException(String message)
+	{
+		super(message);
+	}
+	
+}
